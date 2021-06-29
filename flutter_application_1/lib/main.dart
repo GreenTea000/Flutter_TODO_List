a02a6ca6b6fb41ba706e5d0ff68df96888eef164
paolo.belivi2000@gmail.com <paolo.belivi2000@gmail.com>
2021-06-29 14:38:25 +0300

    first step

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:flutter_application_1/pages/home.dart';
+
+void main() => runApp(MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.deepOrangeAccent,
+      ),
+      home: Home(),
+    ));
